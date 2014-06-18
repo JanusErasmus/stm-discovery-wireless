a3852466b9afe8796fb177da34fe90361901e784
Janus Erasmus <janus@kses.net>
2014-06-18 17:44:18 +0200

    Initial commit

--- wirelessIO/src/version.h
+++ wirelessIO/src/version.h
@@ -1,5 +1,5 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 #define VERSION_NUMBER 0x000100 
-#define BUILD_DATE "Wed Jun 18 17:06:48 SAST 2014" 
+#define BUILD_DATE "Wed Jun 18 17:43:52 SAST 2014" 
 #endif 
