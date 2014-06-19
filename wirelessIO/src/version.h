27fc4969ad1559ba5911c821edb62ad638590f0e
Janus Erasmus <janus@intel.(none)>
2014-06-19 07:54:08 +0200

    Delete unused binaries

--- wirelessIO/src/version.h
+++ wirelessIO/src/version.h
@@ -1,5 +1,5 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 #define VERSION_NUMBER 0x000100 
-#define BUILD_DATE "Wed Jun 18 17:43:52 SAST 2014" 
+#define BUILD_DATE "Thu Jun 19 07:44:33 SAST 2014" 
 #endif 
